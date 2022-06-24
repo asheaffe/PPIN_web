908ec853630cb93072085d0d8a92c62d706d41cb
asheaffe <asheaffe@iwu.edu>
2022-06-24 14:48:23 -0500

    New File

--- python/convert.py
+++ python/convert.py
@@ -2,4 +2,28 @@
 ## Started June 24, 2022  ##
 
 ## python code that will be used to convert ensembl file to json ##
-print("hello")
+def main():
+    content = read_file("human_protein_ids_ensembl106.txt")
+    names = get_gene_name(content)
+    print(names)
+
+# takes in a file name as param and reads the file
+def read_file(file_name):
+    with open(file_name) as f:
+        contents = f.readlines()
+
+    return contents
+
+# takes the contents of a file as str and returns the gene name
+def get_gene_name(contents):
+    # create an empty list
+    names = []
+
+    # loop through the contents passed by param
+    for line in contents[1:]:
+        temp = line.split("\t")
+
+        names.append(temp[0])
+
+    return names
+main()
